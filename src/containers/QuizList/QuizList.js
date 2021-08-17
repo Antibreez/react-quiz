faf6321efe501a70aec6587595fbe5059e6a2586
Anton <kiyananton@gmail.com>
2021-08-17 17:53:04 +0300

    commit 9

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
 import s from './QuizList.scss';
 import {NavLink} from 'react-router-dom';
-import axios from '../../axios/axios-quiz';
+import {connect} from 'react-redux';
+import {fetchQuizes} from '../../store/actions/quiz';
 
 import Loader from '../../components/Ui/Loader/Loader';
 
 class QuizList extends Component {
-    state = { 
-        quizes: [],
-        loading: true,
-     }
 
     renderQuizes() {
-        return this.state.quizes.map(quiz => {
+        return this.props.quizes.map(quiz => {
             return (
                 <li
                     key={quiz.id}
@@ -25,25 +22,27 @@ class QuizList extends Component {
         })
     }
 
-    async componentDidMount() {
-        try {
-            const response = await axios.get('/quizes.json');
+    componentDidMount() {
+        this.props.fetchQuizes();
+        
+        // try {
+        //     const response = await axios.get('/quizes.json');
 
-            const quizes = [];
-            Object.keys(response.data).forEach((key, index) => {
-                quizes.push({
-                    id: key,
-                    name: `Тест №${index + 1}`
-                })
-            });
+        //     const quizes = [];
+        //     Object.keys(response.data).forEach((key, index) => {
+        //         quizes.push({
+        //             id: key,
+        //             name: `Тест №${index + 1}`
+        //         })
+        //     });
 
-            this.setState({
-                quizes,
-                loading: false,
-            })
-        } catch (e) {
-            console.log(e);
-        }
+        //     this.setState({
+        //         quizes,
+        //         loading: false,
+        //     })
+        // } catch (e) {
+        //     console.log(e);
+        // }
     }
 
     render() { 
@@ -53,7 +52,7 @@ class QuizList extends Component {
                     <h1>Список тестов</h1>
 
                     { 
-                        this.state.loading 
+                        this.props.loading
                             ? <Loader /> 
                             : <ul>{ this.renderQuizes() }</ul>
                     }
@@ -63,5 +62,18 @@ class QuizList extends Component {
          );
     }
 }
+
+function mapStateToProps(state) {
+    return {
+        quizes: state.quiz.quizes,
+        loading: state.quiz.loading,
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        fetchQuizes: () => dispatch(fetchQuizes())
+    }
+}
  
-export default QuizList;
+export default connect(mapStateToProps, mapDispatchToProps)(QuizList);
